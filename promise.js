ba0329303f473519ecdd73c8c2838589f0389bde
ZaitsevOleksii <48098161+ZaitsevOleksii@users.noreply.github.com>
2019-04-03 23:43:38 +0300

    Add files via upload

---
+++ promise.js
@@ -0,0 +1,20 @@
+let promise = new Promise(function(resolve, reject){
+	setTimeout(function(){
+		const num = Math.random();
+		if (num > 0.5) {
+			resolve(Math.random());
+			
+		} else {
+			reject('Упс, что-то пошло не так');
+		}
+	}, 1000);
+});
+
+promise
+	.then(function(res){
+		alert(Math.round(res*10));
+	})
+	.catch(function(err){
+		alert(err);
+	})
+	
